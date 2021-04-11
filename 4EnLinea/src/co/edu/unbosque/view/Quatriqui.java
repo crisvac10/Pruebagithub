fa02f217654a12e43c91ed579bfbd4998164d9a3
pc <pc@LAPTOP-RMHCDAPV>
2021-04-11 12:02:28 -0500

    Actualización ventana quatriqui

--- 4EnLinea/src/co/edu/unbosque/view/Quatriqui.java
+++ 4EnLinea/src/co/edu/unbosque/view/Quatriqui.java
@@ -1,5 +1,34 @@
 package co.edu.unbosque.view;
 
-public class Quatriqui {
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class Quatriqui extends JFrame {
+	
+	private int ancho;// Atributo que contiene el ancho de la pantalla
+	private int alto;// Atributo que contiene el alto de la pantalla
+	
+	public Quatriqui() {
+		
+		setTitle("Quatriqui");
+		setSize(800,600);
+		getContentPane().setLayout(null);
+		setIconImage(new ImageIcon(getClass().getResource("/Imagen/logo.png")).getImage());
+		ancho = java.awt.Toolkit.getDefaultToolkit().getScreenSize().width;
+		alto = java.awt.Toolkit.getDefaultToolkit().getScreenSize().height;
+		setLocation((int) (ancho / 5), (int) (alto / 7.5));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		InterfazQuatriqui();
+		
+		setResizable(false);
+		setVisible(false);
+	}
+	
+	public void InterfazQuatriqui() {
+		
+		
+		
+	}
 
 }
