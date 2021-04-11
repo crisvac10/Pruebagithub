3662b88dd2a3efda3a0e5f4e68060e9527360b19
pc <pc@LAPTOP-RMHCDAPV>
2021-04-11 13:05:52 -0500

    Actualización panel Registro de jugadas

--- 4EnLinea/src/co/edu/unbosque/view/PanelRegistro.java
+++ 4EnLinea/src/co/edu/unbosque/view/PanelRegistro.java
@@ -1,5 +1,16 @@
 package co.edu.unbosque.view;
 
-public class PanelRegistro {
+import java.awt.Color;
+import java.awt.FlowLayout;
 
+import javax.swing.JPanel;
+
+public class PanelRegistro extends JPanel{
+
+	
+	public  PanelRegistro() {
+		
+		setLayout(new FlowLayout());
+		setBackground(Color.BLUE);
+	}
 }
