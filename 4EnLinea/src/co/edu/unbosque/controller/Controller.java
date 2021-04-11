0eb4ea3b1d37ca918654b5a6ab680a6565b4dfd1
Cristian Vaca <c.10.alex@hotmail.com>
2021-04-11 14:07:03 -0500

    no se que hice

--- 4EnLinea/src/co/edu/unbosque/controller/Controller.java
+++ 4EnLinea/src/co/edu/unbosque/controller/Controller.java
@@ -5,17 +5,15 @@ import java.awt.event.ActionListener;
 
 import co.edu.unbosque.view.Quatriqui;
 import co.edu.unbosque.view.VentanaBienvenido;
-import co.edu.unbosque.view.VentanaHistorial;
 
 public class Controller implements ActionListener{
 	
 	public VentanaBienvenido ventana_bienvenido;
 	public Quatriqui quatriqui;
-	public VentanaHistorial ventana_historial;
+	
 	
 	public Controller() {
 		ventana_bienvenido = new VentanaBienvenido();
-		ventana_historial = new VentanaHistorial();
 		quatriqui = new Quatriqui();
 		asignarOyentes();
 	}
@@ -24,29 +22,20 @@ public class Controller implements ActionListener{
 		ventana_bienvenido.getBt_Historial().addActionListener(this);
 		ventana_bienvenido.getBt_Jugar().addActionListener(this);
 		
-		quatriqui.getBt_Menú().addActionListener(this);
-		
 	}
 
 	
 	public void actionPerformed(ActionEvent e) {
 		
-		//VENTANA QUATRIQUI
 		if((e.getActionCommand().equals("JUGAR"))){
 			
 			ventana_bienvenido.setVisible(false);
 			quatriqui.setVisible(true);
-		//VENTANA BIENVENIDA		
-		}if((e.getActionCommand().equals("MENÚ"))) {
-			ventana_bienvenido.setVisible(true);
-			quatriqui.setVisible(false);
-			ventana_historial.setVisible(false);
-		
-		//VENTANA HISTORIAL
-		}if((e.getActionCommand().equals("HISTORIAL"))) {
+		}
+		if((e.getActionCommand().equals("HISTORIAL"))) {
 			
 			ventana_bienvenido.setVisible(false);
-			ventana_historial.setVisible(true);
+			quatriqui.setVisible(true);
 		}
 		
 		
--- 4EnLinea/src/co/edu/unbosque/view/PanelRegistro.java
+++ 4EnLinea/src/co/edu/unbosque/view/PanelRegistro.java
@@ -1,16 +1,5 @@
 package co.edu.unbosque.view;
 
-import java.awt.Color;
-import java.awt.FlowLayout;
+public class PanelRegistro {
 
-import javax.swing.JPanel;
-
-public class PanelRegistro extends JPanel{
-
-	
-	public  PanelRegistro() {
-		
-		setLayout(new FlowLayout());
-		setBackground(Color.BLUE);
-	}
 }
