7c36b46f15b1bd43b7f7090efce8589a314b048b
pc <pc@LAPTOP-RMHCDAPV>
2021-04-11 12:04:56 -0500

    Actualización Controller

--- 4EnLinea/src/co/edu/unbosque/controller/Controller.java
+++ 4EnLinea/src/co/edu/unbosque/controller/Controller.java
@@ -3,24 +3,41 @@ package co.edu.unbosque.controller;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import co.edu.unbosque.view.Quatriqui;
 import co.edu.unbosque.view.VentanaBienvenido;
 
 public class Controller implements ActionListener{
 	
 	public VentanaBienvenido ventana_bienvenido;
-	//public Quatriqui nose;
+	public Quatriqui quatriqui;
 	
 	
 	public Controller() {
 		ventana_bienvenido = new VentanaBienvenido();
-		// = Quatriqui
+		quatriqui = new Quatriqui();
+		asignarOyentes();
+	}
+	
+	public void asignarOyentes() {
+		ventana_bienvenido.getBt_Historial().addActionListener(this);
+		ventana_bienvenido.getBt_Jugar().addActionListener(this);
 		
 	}
 
 	
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
 		
-		ventana_bienvenido.getBt_Historial().addActionListener(this);
+		if((e.getActionCommand().equals("JUGAR"))){
+			
+			ventana_bienvenido.setVisible(false);
+			quatriqui.setVisible(true);
+		}
+		if((e.getActionCommand().equals("HISTORIAL"))) {
+			
+			ventana_bienvenido.setVisible(false);
+			quatriqui.setVisible(true);
+		}
+		
 		
 	}
 	
