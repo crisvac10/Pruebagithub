f6bcab694902647736bafa12234a4db2471b99cc
Cristian Vaca <c.10.alex@hotmail.com>
2021-04-12 08:14:30 -0500

    panel registro

--- 4EnLinea/src/co/edu/unbosque/view/PanelRegistro.java
+++ 4EnLinea/src/co/edu/unbosque/view/PanelRegistro.java
@@ -3,14 +3,15 @@ package co.edu.unbosque.view;
 import java.awt.Color;
 import java.awt.FlowLayout;
 
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.border.TitledBorder;
 
 public class PanelRegistro extends JPanel{
 
-	private JTextField Jugador1;
-	private JTextField Jugador2;
+	private JLabel area_texto;
 	
 	
 	public  PanelRegistro() {
@@ -21,5 +22,14 @@ public class PanelRegistro extends JPanel{
 		
 		
 		
-	}
+		funcionar();
+		}
+		
+		public void funcionar() {
+			area_texto = new JLabel();
+			area_texto.setBounds(420, 100, 400, 600);
+			JScrollPane panel = new JScrollPane(area_texto);
+			panel.setBounds(420, 100, 400, 600);
+			add(panel);
+		}
 }
